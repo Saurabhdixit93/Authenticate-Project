9846cd88c0e3ea812b8c49932e6706327fc48911
Saurabh Dixit <86460997+Saurabhdixit93@users.noreply.github.com>
2024-05-27 21:57:06 +0530

    Update contactController.js

--- Core_files/Controller/contactController.js
+++ Core_files/Controller/contactController.js
@@ -1,75 +1,94 @@
-const { Op, Sequelize } = require("sequelize");
+const { Op } = require("sequelize");
 const Contact = require("../Models/contact");
 const User = require("../Models/user");
+const SpamReport = require("../Models/spamReport");
 
-exports.markAsSpam = async (req, res) => {
-  const { phoneNumber } = req.body;
-  if (!phoneNumber)
-    return res.status(400).json({ error: "Phone number is required" });
-
+exports.reportSpam = async (req, res) => {
   try {
-    // here check if already exists with isSpam true return with message "Already marked as spam"
+    const { phoneNumber } = req.body;
+    const userId = req.UserId;
 
+    if (!phoneNumber) {
+      return res.status(400).json({ error: "Phone number is required" });
+    }
+
+    // same number with same user report will be ignored
+    const spamReport = await SpamReport.findOne({
+      where: { phoneNumber, reportedBy: userId },
+    });
+    if (spamReport) {
+      return res.status(400).json({ error: "Spam report already exists" });
+    }
+
+    // create spam report
+    await SpamReport.create({ phoneNumber, reportedBy: userId });
+
+    // and also update the isSpam field in the Contact model
     const contact = await Contact.findOne({ where: { phoneNumber } });
     if (contact) {
-      if (contact.isSpam) {
-        return res.status(400).json({ error: "Already marked as spam" });
-      }
       contact.isSpam = true;
       await contact.save();
-    } else {
-      await Contact.create({ phoneNumber, isSpam: true });
     }
-    return res.json({ message: "Marked as spam" });
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
+    return res.status(201).json({ message: "Spam reported successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
-
 exports.searchByName = async (req, res) => {
   try {
     const { name } = req.body;
 
+    // starts with name
     const startsWithName = await User.findAll({
       where: {
         name: {
           [Op.iLike]: `${name}%`,
         },
       },
-      attributes: ["name", "phoneNumber"],
+      attributes: ["id", "name", "phoneNumber"],
+      include: { model: SpamReport, attributes: [] },
     });
 
+    // contains name
     const containsName = await User.findAll({
       where: {
         name: {
           [Op.and]: {
             [Op.notLike]: `${name}%`,
-            [Op.like]: `%${name}%`,
+            [Op.iLike]: `%${name}%`,
           },
         },
       },
-      attributes: ["name", "phoneNumber"],
+      attributes: ["id", "name", "phoneNumber"],
+      include: { model: SpamReport, attributes: [] },
     });
+
     // Combine and send the search results
     const searchResults = startsWithName.concat(containsName);
-    return res.json({ searchResults });
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
+
+    if (searchResults.length === 0) {
+      return res.status(404).json({ message: "No users found" });
+    }
+
+    return res.status(200).json({ searchResults });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
 };
-
 exports.searchByPhoneNumber = async (req, res) => {
-  const { phoneNumber } = req.body;
   try {
-    const contacts = await Contact.findAll({
+    const { phoneNumber } = req.body;
+    const users = await User.findAll({
       where: { phoneNumber },
-      attributes: ["name", "phoneNumber"],
+      attributes: ["id", "name", "phoneNumber"],
+      include: { model: SpamReport, attributes: [] },
     });
-    if (contacts.length === 0) {
-      return res.status(404).json({ message: "No contacts found" });
+
+    if (users.length === 0) {
+      return res.status(404).json({ message: "No users found" });
     }
-    return res.json({ data: contacts, message: "Contacts found" });
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
+    return res.status(200).json({ users });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
 };
