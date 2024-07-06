fbe682afd27e7d899bfb77c9d5cb378691e7b97c
Saurabh Dixit <86460997+Saurabhdixit93@users.noreply.github.com>
2024-07-06 12:06:48 +0530

    Update index.js

--- Core_files/Routes/index.js
+++ Core_files/Routes/index.js
@@ -4,6 +4,9 @@ const contactRouter = require("./contactRoute");
 const userRouter = require("./userRoutes");
 const ValidateAuthUser = require("../Middleware/AuthRequire");
 
+router.get("/", (req, res) => {
+  return res.status(200).json({ message: "Server Running Successfully" });
+});
 router.use("/contact", ValidateAuthUser, contactRouter);
 router.use("/user", userRouter);
 
