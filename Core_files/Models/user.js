b54a0f3d4acb2699ee5e2abc7bd82d59655b8409
Saurabh Dixit <86460997+Saurabhdixit93@users.noreply.github.com>
2024-07-06 15:47:18 +0530

    Update user.js

--- Core_files/Models/user.js
+++ Core_files/Models/user.js
@@ -1,29 +1,70 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../../config/database");
+// const { Model, DataTypes } = require("sequelize");
+// const sequelize = require("../../config/database");
 
-class User extends Model {}
-User.init(
-  {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    phoneNumber: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
+// class User extends Model {}
+// User.init(
+//   {
+//     name: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+// phoneNumber: {
+//   type: DataTypes.STRING,
+//   unique: true,
+//   allowNull: false,
+// },
+//     email: {
+//       type: DataTypes.STRING,
+//       unique: true,
+//       allowNull: true,
+//     },
+//     password: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//   },
+//   { sequelize, modelName: "user_db" }
+// );
+
+// module.exports = User;
+
+// models/user.js
+
+const { DataTypes } = require("sequelize");
+const sequelize = require("../../config/database");
+const Contact = require("./contact");
+
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    unique: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
     },
   },
-  { sequelize, modelName: "user_db" }
-);
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phoneNumber: {
+    type: DataTypes.STRING,
+    unique: true,
+    // allowNull: false,
+  },
+});
+
+User.hasMany(Contact);
+Contact.belongsTo(User);
 
 module.exports = User;
