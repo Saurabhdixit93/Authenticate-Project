8ea218ac73a7ce559b2c897ebd1a8ebc44b26ebc
Saurabh Dixit <86460997+Saurabhdixit93@users.noreply.github.com>
2024-07-06 15:46:07 +0530

    Update contactController.js
    Update contact controller

--- Core_files/Controller/contactController.js
+++ Core_files/Controller/contactController.js
@@ -1,76 +1,68 @@
 const { Op } = require("sequelize");
 const Contact = require("../Models/contact");
 const User = require("../Models/user");
-const SpamReport = require("../Models/spamReport");
 
 exports.reportSpam = async (req, res) => {
+  const { phoneNumber } = req.body;
+  const userId = req.userId;
+
   try {
-    const { phoneNumber } = req.body;
-    const userId = req.UserId;
+    // Check if the phone number already exists in contacts
+    let contact = await Contact.findOne({ where: { phoneNumber } });
 
-    if (!phoneNumber) {
-      return res.status(400).json({ error: "Phone number is required" });
-    }
-
-    // same number with same user report will be ignored
-    const spamReport = await SpamReport.findOne({
-      where: { phoneNumber, reportedBy: userId },
-    });
-    if (spamReport) {
-      return res.status(400).json({ error: "Spam report already exists" });
-    }
-
-    // create spam report
-    await SpamReport.create({ phoneNumber, reportedBy: userId });
-
-    // and also update the isSpam field in the Contact model
-    const contact = await Contact.findOne({ where: { phoneNumber } });
-    if (contact) {
+    // If the contact doesn't exist, create a new one
+    if (!contact) {
+      contact = await Contact.create({ phoneNumber, isSpam: true });
+    } else {
       contact.isSpam = true;
+      contact.reportBy = userId;
       await contact.save();
     }
-    return res.status(201).json({ message: "Spam reported successfully" });
+
+    return res
+      .status(200)
+      .json({ message: "Phone number reported as spam successfully." });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    return res
+      .status(500)
+      .json({ message: "Failed to report phone number as spam." });
   }
 };
+
 exports.searchByName = async (req, res) => {
   try {
     const { name } = req.body;
 
-    // starts with name
-    const startsWithName = await User.findAll({
+    const users = await User.findAll({
       where: {
-        name: {
-          [Op.iLike]: `${name}%`,
-        },
+        name: { [Op.iLike]: `%${name}%` },
       },
-      attributes: ["id", "name", "phoneNumber"],
-      include: { model: SpamReport, attributes: [] },
+      attributes: ["name", "phoneNumber", "id"],
     });
 
-    // contains name
-    const containsName = await User.findAll({
-      where: {
-        name: {
-          [Op.and]: {
-            [Op.notLike]: `${name}%`,
-            [Op.iLike]: `%${name}%`,
+    const phoneNumbers = await Promise.all(
+      users.map(async (user) => {
+        const contacts = await Contact.findAll({
+          attributes: ["phoneNumber", "isSpam", "id"],
+          where: {
+            phoneNumber: user?.phoneNumber, // Filter contacts by phoneNumber
           },
-        },
-      },
-      attributes: ["id", "name", "phoneNumber"],
-      include: { model: SpamReport, attributes: [] },
-    });
-
-    // Combine and send the search results
-    const searchResults = startsWithName.concat(containsName);
-
-    if (searchResults.length === 0) {
+        });
+        if (contacts.length === 0) return user;
+        return contacts.map((contact) => ({
+          user,
+          phoneNumber: contact.phoneNumber,
+          isSpam: contact.isSpam,
+        }));
+      })
+    );
+    // Flatten the array of phone numbers
+    const flattenedPhoneNumbers = phoneNumbers.flat();
+    if (phoneNumbers.length === 0) {
       return res.status(404).json({ message: "No users found" });
     }
 
-    return res.status(200).json({ searchResults });
+    return res.status(200).json({ data: flattenedPhoneNumbers });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
@@ -78,16 +70,37 @@ exports.searchByName = async (req, res) => {
 exports.searchByPhoneNumber = async (req, res) => {
   try {
     const { phoneNumber } = req.body;
-    const users = await User.findAll({
-      where: { phoneNumber },
-      attributes: ["id", "name", "phoneNumber"],
-      include: { model: SpamReport, attributes: [] },
+    const contactsDetails = await Contact.findAll({
+      where: {
+        phoneNumber: { [Op.iLike]: `%${phoneNumber}%` },
+      },
+      attributes: ["isSpam", "phoneNumber", "id"],
     });
+    const phoneNumbers = await Promise.all(
+      contactsDetails.map(async (contact) => {
+        const users = await User.findAll({
+          attributes: ["phoneNumber", "name", "email", "id"],
+          where: {
+            phoneNumber: contact?.phoneNumber, // Filter contacts by phoneNumber
+          },
+          required: false,
+        });
 
-    if (users.length === 0) {
+        if (users?.length === 0) {
+          return contact;
+        }
+        return users.map((specificUser) => ({
+          contact,
+          user: specificUser,
+        }));
+      })
+    );
+    // Flatten the array of phone numbers
+    const flattenedPhoneNumbers = phoneNumbers.flat();
+    if (phoneNumbers.length === 0) {
       return res.status(404).json({ message: "No users found" });
     }
-    return res.status(200).json({ users });
+    return res.status(200).json({ data: flattenedPhoneNumbers });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
